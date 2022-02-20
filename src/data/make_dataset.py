3003824dbc00b130f7ab10f43d46e737c9e6e8f3
KatharinaDanziger <katharina.danziger@posteo.de>
2022-02-20 13:14:27 +0100

    Added first draft

---
+++ src/data/make_dataset.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# *_* coding: utf-8 *_*
+
+"""
+module docstring - short summary
+"""
+
+__version__ = "1.0.0"
+__author__ = "K4th4r1n4"
+
+# --------------------------------------------------------------------------------
+
+# import stuff
+
+
+def main(args=None):
+    pass
+
+
+
+if "__main__" == __name__:
+    import sys
