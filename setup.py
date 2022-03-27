8dcfb9085c6142af6843c0c4903ec6fec9732a69
KatharinaDanziger <katharina.danziger@posteo.de>
2022-03-27 16:59:08 +0200

    Added setup.py and LICENSE

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="1.0.0",
+    packages=find_packages(),
+    license="MIT",
+    author="me",
+    long_description=open("README.md").read()
+)
